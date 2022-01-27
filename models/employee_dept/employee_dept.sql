94f3b5148b2e4010bda102f3a2b7e79a8a76be51
sanjana.moghe@gmail.com <sanjana.moghe@gmail.com>
2022-01-27 07:43:20 +0000

    emp

--- models/employee_dept/employee_dept.sql
+++ models/employee_dept/employee_dept.sql
@@ -3,6 +3,6 @@
 with source_data as (
   select * from {{ source('employee_dept', 'employee') }} 
 ),
-final as (select emp_id,emp_name,dept_id from source_data
-inner join {{ source('employee_dept', 'department') }})
+final as (select emp_id,emp_name,{{ source('employee_dept', 'department') }}.dept_id,{{ source('employee_dept', 'department') }}.dept_name from source_data
+ join {{ source('employee_dept', 'department') }} using (dept_id))
 select * from final;
